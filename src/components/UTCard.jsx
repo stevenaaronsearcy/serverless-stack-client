deb89637e1e0dbe672f58be7b14c93e6e463243e
Steven Searcy <stevenaaronsearcy@gmail.com>
2021-06-21 17:48:50 -0500

    Portfolio Edit Fix

--- src/components/UTCard.jsx
+++ src/components/UTCard.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import "./UTCard.css";
 import UT from "../assets/UT.png";
-
 
 const useStyles = makeStyles({
   root: {
